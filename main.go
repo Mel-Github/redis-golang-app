f59b5a38ecbfd4177af53cdf01c592991f429ea0
Mel-GitHub <mel000vin@yahoo.com>
2020-05-10 18:25:36 +0800

    circleci bot

--- main.go
+++ main.go
@@ -31,12 +31,22 @@ func incrementCounter() int {
 func redisIncrement() int {
 
 	conn, err := redis.Dial("tcp", "redis-node:6379")
+	//conn, err := redis.Dial("tcp", "localhost6379")
 	if err != nil {
 		log.Fatal().Err(err).Msg("Unable to connect to redis")
 	}
 	// Importantly, use defer to ensure the connection is always
 	// properly closed before exiting the main() function.
 	defer conn.Close()
+
+	// Retrieve Redis counter
+	previous, err := redis.Int(conn.Do("GET", "mycounter"))
+	//reply, err := redis.StringMap(conn.Do("GET", "mycounter"))
+	if err != nil {
+		log.Fatal().Err(err).Msg("Error retrieving album")
+	}
+
+	log.Info().Str("Before increment current counter value", strconv.Itoa(previous)).Msg("Previous value")
 
 	// Incrementing Redis counter
 	_, err = conn.Do("INCR", "mycounter")
@@ -52,7 +62,7 @@ func redisIncrement() int {
 		log.Fatal().Err(err).Msg("Error retrieving album")
 	}
 
-	log.Info().Str("Current counter value", strconv.Itoa(reply)).Msg("Current counter value successfully retrieved")
+	log.Info().Str("Current counter value", strconv.Itoa(reply)).Msg("Current value")
 
 	return reply
 
@@ -85,12 +95,16 @@ func main() {
 	tmpl := template.Must(t, nil)
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Print("Enter handler")
 
 		visitorcounter := incrementCounter()
+
+		log.Print("Inside handler after increment before template execution")
 		myvar := map[string]interface{}{
 			"MyVar": visitorcounter,
 		}
 		tmpl.Execute(w, myvar)
+		log.Print("Inside handler after template execution")
 	})
 
 	// Simple health check to integration with Kubernetes
